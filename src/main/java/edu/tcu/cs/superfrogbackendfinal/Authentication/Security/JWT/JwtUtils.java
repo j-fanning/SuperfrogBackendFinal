7ddc5a64ec1ff70e00918d9d7ce1697450d2565d
James Fanning <fanning07@cox.net>
2022-05-01 19:25:21 -0500

    test

--- src/main/java/edu/tcu/cs/superfrogbackendfinal/Authentication/Security/JWT/JwtUtils.java
+++ src/main/java/edu/tcu/cs/superfrogbackendfinal/Authentication/Security/JWT/JwtUtils.java
@@ -37,7 +37,7 @@ public class JwtUtils {
 
     public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
         String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-        ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).sameSite("none").build();
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).sameSite("lax").build();
         return cookie;
     }
 
